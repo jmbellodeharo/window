ea6e8bbc3a985f17b8d44f8bdb14186e09625190
jmbellodeharo <josemariabellodeharodev@gmail.com>
2020-08-19 20:42:42 +0200

    Added fish with tails

--- bubbles.js
+++ bubbles.js
@@ -1,11 +1,15 @@
 window.addEventListener("load", first)
 var bubbleQuantity = 1000;
+var fishQuantity = 40;
 var maxSize = 3;
+var maxSizeFish = 8;
+var velocityMultiplierFish = 4;
 var velocityMultiplier = 4;
 var RG = 0;
 var darkness = 80;
 var randomMovement = 2;
 var bubbleArray = new Array();
+var fishArray = new Array();
 var canvas;  
 var ctx;
 
@@ -35,7 +39,7 @@ function first(){
 
     bubbleArray = getDrops(bubbleQuantity)
 
-    console.log(bubbleArray)
+    fishArray = createFish(fishQuantity);
 
     //Water sound
     var audioElement = document.createElement("audio");
@@ -80,6 +84,17 @@ function draw (){
         ctx.fill();
         
     }
+
+    for (let i = 0; i < fishArray.length; i++) {
+        
+        // Position fish
+        fishArray[i].move();
+        // Draw fish:size
+        fishArray[i].draw();
+        ctx.fill();
+        
+    }
+
     
     window.requestAnimationFrame(draw);
 
@@ -129,22 +144,45 @@ function getDrops(quantity, existentBubbles=undefined){
     return bubbles
 }
 
+function createFish(quantity, existingFish=undefined){
+    var fishes;
+    
+    if(existingFish){
+        if(quantity > existingFish.length){
+            // When the number of desired fishes is higher than existent quantity i add new bubbles
+            for(;quantity > existingFish.length;quantity--){
+                existingFish.push(new fish())}
+        }else{
+            // When the number of desired fishes is lesser to the existent quantity i remove existent bubbles
+            for(;quantity < existingFish.length;quantity++){
+                existingFish.pop();
+            }
+        }
+        fishes = existingFish;
+    }else{
+        fishes = new Array();
+        for(var i = 0; i < quantity; i++){
+            fishes.push(new fish())}
+    }
+    
+    return fishes
+}
 
 class bubble {
     constructor(x, y, r, g, b, a, radio, angleS, angleE) {
-      this.x = x;
-      this.y = y;
-      this.r = r;
-      this.g = g;
-      this.b = b;
-      this.a = a;
-      this.radio = radio;
-      this.angleS = angleS;
-      this.angleE = angleE;
-      this.movement = Math.random() * velocityMultiplier;
-      // To save last 20 points bubbles points
-      /*this.points = [];*/
-      //this.dropSound = new Audio("./sounds/drop1.mp3"); // Initializate sound
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+        this.radio = radio;
+        this.angleS = angleS;
+        this.angleE = angleE;
+        this.movement = Math.random() * velocityMultiplier;
+        // To save last 20 points bubbles points
+        /*this.points = [];*/
+        //this.dropSound = new Audio("./sounds/drop1.mp3"); // Initializate sound
     }
 
     drawBubble(bubbleSize){
@@ -183,3 +221,82 @@ class bubble {
     }
   };
 
+class fish {
+    constructor() {
+        this.direction = Math.random() > 0.5 ? 1: -1; // -1 left, 1 right
+        this.movement = Math.random() * velocityMultiplierFish * this.direction;
+        if(this.direction == 1){
+            this.x = -100;
+        }else{
+            this.x = canvas.width + 100;
+        }
+        
+        this.y = Math.floor(Math.random() * canvas.height);
+        this.size = Math.floor(Math.random() * maxSizeFish);
+        this.r = Math.floor(Math.random() * 255)
+        this.g = Math.floor(Math.random() * 255)
+        this.b = Math.floor(Math.random() * 255)
+    }
+    
+    move(){
+        this.x += this.movement;
+        if(this.x > canvas.width + 100 || this.x < -100){
+            if(this.x > canvas.width + 100){
+                this.direction = -1;
+            }else if(this.x < -100){
+                this.direction = 1;
+            }
+            
+            this.movement *= -1;
+        }
+    }
+    draw(){
+        var centerX = 0;
+        var centerY = 0;
+        var radius = this.size;
+  
+        // save state
+        ctx.save();
+ 
+        //BODY
+        // translate context
+        ctx.translate(this.x, this.y);
+  
+        // scale context horizontally
+        ctx.scale(this.size*2, this.size);
+  
+        // draw circle which will be stretched into an oval
+        ctx.beginPath();
+        ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+  
+        // apply styling
+        ctx.fillStyle = "rgb("+this.r+","+this.g+","+this.b+")";
+        ctx.fill();
+        //*********
+        //EYE
+        ctx.beginPath();
+
+        // Returning eye size to the initial scale
+        ctx.scale(this.size/2, this.size);
+        
+        // The position is relative to the scale, don't need to calculate weird positions
+        ctx.arc(this.direction, -0.5, this.size/50, 0, 2 * Math.PI, false);
+        
+        ctx.fillStyle = "black";
+        ctx.fill();
+
+        // Tail, i want the reverse direction
+        ctx.moveTo(-2*this.direction, 0);
+        ctx.lineTo(-3*this.direction, 1);
+        ctx.lineTo(-3*this.direction, -1);
+
+        ctx.fillStyle = "grey";
+        ctx.fill();
+        
+        // ctx.stroke is for the line that envolves the shape
+        //*********
+
+        // restore to original state
+        ctx.restore();
+    }
+};
